3114fb417f3fb825f133624b8a1b03390fcedeeb
Loreto <0314lsolanes@e-itaca.es>
2020-10-06 11:14:32 +0200

    Enums
    Aun por terminar el punto2

--- src/edu/facturacion/Producto.java
+++ src/edu/facturacion/Producto.java
@@ -2,21 +2,33 @@ package edu.facturacion;
 
 public class Producto {
 
-	private double importe;
-	private String descripcion;
-	
 	private enum IVA {
-		SUPERREDUCIDO ("Super reducido", 4), 
-		REDUCIDO ("Reducido", 10), 
-		NORMAL ("Normal", 21);
+		SUPERREDUCIDO 	("Super reducido", 4), 
+		REDUCIDO 		("Reducido", 10), 
+		NORMAL 			("Normal", 21);
 		
 		private final String nombreTipoIVA;
 		private final int porcentaje;
 		
-		IVA(String nombreTipoIVA, int porcentaje){
-			
+		private IVA(String nombreTipoIVA, int porcentaje){
+			this.nombreTipoIVA = nombreTipoIVA;
+			this.porcentaje = porcentaje;
 		}
+		
+		public String getNombreTipoIVA() {
+			return nombreTipoIVA;
+		}
+		
+		public int getPorcentaje() {
+			return porcentaje;
+		}
+		
 	}; //FIN ENUM
+	
+	private double importe;
+	private String descripcion;
+	private IVA tipoIVA;
+	
 	
 //CONSTRUCTOR
 	public Producto(double importe, String descripcion) {
